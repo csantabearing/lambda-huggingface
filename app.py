61c9ba3183938cc10c4d0bd6bb5a8059e27896c9
csantabearing <camilo@bearing.ai>
2022-06-26 18:08:02 -0500

    SAM

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ model_path='./model'
 classify = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
 
 def handler(event, context):
+    #return json.dumps(event)
     body = event['body']
     queries = json.loads(body)['query']
     result={'result':[classify(x) for x in queries]}
