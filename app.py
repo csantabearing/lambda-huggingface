5c7947031addf6247be0f89ab2e04f494308e823
csantabearing <camilo@bearing.ai>
2022-06-27 00:35:08 -0500

    Response schema

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ classify = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path
 def handler(event, context):
     body = event['body']
     queries = json.loads(body)['query']
-    result={'result':[classify(x) for x in queries]}
-    return json.dumps(result)
+    sentiments={'result':[classify(x) for x in queries]}
+    result={
+        "isBase64Encoded": True,
+        "statusCode": 200,
+        "headers": {'model_path':model_path},
+        "body": json.dumps(sentiments)
+    }  
+    return result
