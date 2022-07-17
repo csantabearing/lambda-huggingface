b8d700c659348018b6dfde93dc569bdf6525b03c
csantabearing <camilo@bearing.ai>
2022-07-17 00:35:44 -0500

    Magnum

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from transformers import pipeline
+from fastapi import FastAPI
+from mangum import Mangum
 import json
-model_path='./model'
+
+model_path = './model'
 classify = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
 
-def handler(event, context):
+app = FastAPI(title='Serverless Lambda FastAPI')
+
+
+def other_handler(event, context):
     body = event['body']
     queries = json.loads(body)['query']
-    sentiments={'result':[classify(x) for x in queries]}
-    result={
+    sentiments = {'result': [classify(x) for x in queries]}
+    result = {
         "isBase64Encoded": True,
         "statusCode": 200,
-        "headers": {'model_path':model_path},
+        "headers": {
+            'model_path': model_path
+        },
         "body": json.dumps(sentiments)
-    }  
+    }
     return result
+
+
+@app.get("/", tags=["Endpoint Test"])
+def main_endpoint_test():
+    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
+
+
+handler = Mangum(app=app)
